035d1cc38478a380c1882e49ed66b93de5bd6758
vinit <vinitkkulkarni@gmail.com>
2023-04-28 19:55:42 +0530

    Added:this class to hold data for root,left,right tree elements

---
+++ MyBinaryNode.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.binarysearchtree;
+
+public class MyBinaryNode<T extends Comparable<T>> {
+    T data;
+    MyBinaryNode<T> left;
+    MyBinaryNode<T> right;
+
+    public MyBinaryNode(T data){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
